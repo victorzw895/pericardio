972052aa28c3123d32c6c4733aed57b6bc861574
Victor Zhang <victorzw895@gmail.com>
2020-09-22 22:06:14 +1000

    Updates

---
+++ precache-manifest.d4351fa219ed25c47bd3ab63b067ce5a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a02ee153de26e854393905eb200776b",
+    "url": "/pericardio/index.html"
+  },
+  {
+    "revision": "21a597ace51c2480fb1c",
+    "url": "/pericardio/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "8af8bae7452dd1922588",
+    "url": "/pericardio/static/css/main.957c666a.chunk.css"
+  },
+  {
+    "revision": "21a597ace51c2480fb1c",
+    "url": "/pericardio/static/js/2.0e0b1d2c.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/pericardio/static/js/2.0e0b1d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af8bae7452dd1922588",
+    "url": "/pericardio/static/js/main.b7485cea.chunk.js"
+  },
+  {
+    "revision": "fecd9b95f9c779f53523",
+    "url": "/pericardio/static/js/runtime-main.64267aa8.js"
+  },
+  {
+    "revision": "921351f146d78d55c8030239527bf2d6",
+    "url": "/pericardio/static/media/FuturaStd-Book.921351f1.otf"
+  },
+  {
+    "revision": "c961c9b1802e79ea12dcfe89bd016753",
+    "url": "/pericardio/static/media/FuturaStd-BookOblique.c961c9b1.otf"
+  },
+  {
+    "revision": "14792084dfffdbe3116c33b3a8c5c5f9",
+    "url": "/pericardio/static/media/FuturaStd-Medium.14792084.otf"
+  },
+  {
+    "revision": "795ac94491a2d870483879cf4d3d2f13",
+    "url": "/pericardio/static/media/FuturaStd-MediumOblique.795ac944.otf"
+  },
+  {
+    "revision": "2e99e496701618c331dfe3e3683ca7e8",
+    "url": "/pericardio/static/media/OptimaLTStd-Medium.2e99e496.otf"
+  },
+  {
+    "revision": "cf72ea468291580b6af52e8c0dc2c970",
+    "url": "/pericardio/static/media/OptimaLTStd-MediumItalic.cf72ea46.otf"
+  }
+]);
